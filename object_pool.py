c4f729ee2f792a9edadd04f79f438d4f98002845
Jacob Aukeman <jacob@jacobaukeman.com>
2015-11-11 20:48:32 -0500

    added object pool

---
+++ object_pool.py
@@ -0,0 +1,16 @@
+class ObjectPool:
+    
+    def __init__(self, size, pooled_type, *init_args):
+        self.pool=[ pooled_type(*init_args) for i in range(0,size) ]
+        self.current_idx=0
+
+    def get(self,*args):
+        result=self.pool[ self.current_idx ]
+
+        self.current_idx += 1
+        if self.current_idx == len(self.pool):
+            self.current_idx=0
+
+        result.__init__(*args)
+        return result
+        
